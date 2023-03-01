1b18733d568ac972cd6f4973259af0f622aa515c
Ryan Izac <ryanizac.infor@gmail.com>
2023-03-01 15:39:59 -0300

    lib: extract basepath

--- src/lib/controller/metadata/metadata.ts
+++ src/lib/controller/metadata/metadata.ts
@@ -4,15 +4,23 @@ import { ControllerOptions } from "./options";
 
 export class ControllerMetadata<C extends IController> {
   private Controller: C;
+  private basepath: string;
 
   private constructor(Controller: C, options?: ControllerOptions) {
     const finalOptions = this.extractOptions(options);
 
+    const basepath = finalOptions.basepath || this.extractBasepath(Controller);
+
     this.Controller = Controller;
+    this.basepath = basepath;
   }
 
-  extractOptions(options?: ControllerOptions) {
+  extractOptions(options?: ControllerOptions): ControllerOptions {
     return { ...options };
+  }
+
+  extractBasepath(Controller: C): string {
+    return "/" + Controller.name.replace(/Controller$/, "").toLocaleLowerCase();
   }
 
   static Define<C extends IController>(
